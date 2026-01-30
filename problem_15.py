7fdc6303c1433affffed3b8eb60a51e68706bc48
rithikroeshan786-cpu <rithikroeshan786@gmail.com>
2026-01-30 13:26:20 +0530

    Update problem_15.py

--- problem_15.py
+++ problem_15.py
@@ -2,9 +2,10 @@
 # Find and fix the error
 
 def celsius_to_fahrenheit(celsius):
-    fahrenheit = celsius * 9/5 + 32
+    fahrenheit = (celsius * 9/5) + 32
     return fahrenheit
 
 temp_c = 25
 temp_f = celsius_to_fahrenheit(temp_c)
+
 print(f"{temp_c}°C = {temp_f}°F")
