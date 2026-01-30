d0b76da45f216867427bae2095340e7b7dde6d30
rithikroeshan786-cpu <rithikroeshan786@gmail.com>
2026-01-30 14:50:46 +0530

    Update problem_42.py

--- problem_42.py
+++ problem_42.py
@@ -3,6 +3,9 @@
 
 words = ["Hello", "World", "Python"]
 sentence = ""
+
 for word in words:
     sentence += word + " "
+
+sentence = sentence.strip()   # remove trailing space
 print(f"Sentence: {sentence}")
