028a9ccb908146e739d63d1df2f45bb4d2b11856
rithikroeshan786-cpu <rithikroeshan786@gmail.com>
2026-01-30 14:57:00 +0530

    Update problem_42.py

--- problem_42.py
+++ problem_42.py
@@ -2,7 +2,11 @@
 # Find and fix the error
 
 words = ["Hello", "World", "Python"]
+
 sentence = ""
 for word in words:
     sentence += word + " "
+
+sentence = sentence.strip()   # remove extra space at the end
+
 print(f"Sentence: {sentence}")
