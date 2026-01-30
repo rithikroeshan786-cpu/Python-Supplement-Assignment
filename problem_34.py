1ed12cbbf62c22bb8b3be27de60aa11db2fb0288
rithikroeshan786-cpu <rithikroeshan786@gmail.com>
2026-01-30 14:24:41 +0530

    Update problem_34.py

--- problem_34.py
+++ problem_34.py
@@ -1,8 +1,8 @@
 # Problem 34: Copy a list
 # Find and fix the error
-
 original = [1, 2, 3, 4, 5]
-copy = original
+copy = original[:]  # makes a copy
 copy.append(6)
+
 print(f"Original: {original}")
 print(f"Copy: {copy}")
