1e1af697d3a2a9b487f4880d1c21ec5e834be22f
rithikroeshan786-cpu <rithikroeshan786@gmail.com>
2026-01-30 19:56:59 +0530

    Update problem_05.py

--- problem_05.py
+++ problem_05.py
@@ -2,9 +2,11 @@
 # Find and fix the error
 
 text = "Hello World"
-vowels = "aeiou"
+vowels = "aeiouAEIOU"
 count = 0
+
 for char in text:
     if char in vowels:
         count += 1
+
 print(f"Number of vowels: {count}")
