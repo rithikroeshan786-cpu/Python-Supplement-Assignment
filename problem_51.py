3ea3fba24266b2320e8e10795da9ead9387bd025
rithikroeshan786-cpu <rithikroeshan786@gmail.com>
2026-01-30 15:39:12 +0530

    Update problem_51.py

--- problem_51.py
+++ problem_51.py
@@ -4,8 +4,10 @@
 def reverse_words(sentence):
     words = sentence.split()
     reversed_words = []
+
     for word in words:
         reversed_words.append(word[::-1])
+
     return " ".join(reversed_words)
 
 text = "Hello World"
